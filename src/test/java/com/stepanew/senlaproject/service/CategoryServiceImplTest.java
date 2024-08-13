d4a5c25894175af99c256b5f5e768dad95cf62f4
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-08-13 21:32:45 +0300

    feat:added tests for storeService, userService, authService, categoryService

---
+++ src/test/java/com/stepanew/senlaproject/service/CategoryServiceImplTest.java
@@ -0,0 +1,239 @@
+package com.stepanew.senlaproject.service;
+
+import com.stepanew.senlaproject.domain.dto.request.CategoryCreateRequestDto;
+import com.stepanew.senlaproject.domain.dto.request.CategoryUpdateRequestDto;
+import com.stepanew.senlaproject.domain.dto.response.CategoryResponseDto;
+import com.stepanew.senlaproject.domain.entity.Category;
+import com.stepanew.senlaproject.domain.mapper.category.CategoryCreateRequestDtoMapper;
+import com.stepanew.senlaproject.domain.mapper.category.CategoryResponseDtoMapper;
+import com.stepanew.senlaproject.exceptions.CategoryException;
+import com.stepanew.senlaproject.repository.CategoryRepository;
+import com.stepanew.senlaproject.services.impl.CategoryServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private CategoryResponseDtoMapper categoryResponseDtoMapper;
+
+    @Mock
+    private CategoryCreateRequestDtoMapper categoryCreateRequestDtoMapper;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
+
+    private final static Long DEFAULT_ID = 1L;
+
+    private final static String DEFAULT_NAME = "NAME";
+
+    private final static String DEFAULT_DESCRIPTION = "DESCRIPTION";
+
+    @Test
+    void findById() {
+        Category category = createCategory();
+        CategoryResponseDto expectedResponse = createCategoryResponseDto();
+
+        when(categoryRepository
+                .findById(DEFAULT_ID))
+                .thenReturn(Optional.of(category));
+        when(categoryResponseDtoMapper
+                .toDto(category))
+                .thenReturn(expectedResponse);
+
+        CategoryResponseDto testCategory = categoryService.findById(DEFAULT_ID);
+
+        verify(categoryRepository).findById(DEFAULT_ID);
+        verify(categoryResponseDtoMapper).toDto(category);
+        Assertions.assertEquals(expectedResponse, testCategory);
+    }
+
+    @Test
+    void findByIdThrowsException() {
+        when(categoryRepository
+                .findById(DEFAULT_ID))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(
+                CategoryException.class, () -> categoryService.findById(DEFAULT_ID)
+        );
+    }
+
+    @Test
+    void findAll() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Category category = createCategory();
+        Page<Category> categoriesPage = new PageImpl<>(Collections.singletonList(category));
+        CategoryResponseDto expectedResponse = new CategoryResponseDto(DEFAULT_ID, null, null);
+
+        when(categoryRepository
+                .findAllByNameContainingIgnoreCase("", pageable))
+                .thenReturn(categoriesPage);
+        when(categoryResponseDtoMapper
+                .toDto(category))
+                .thenReturn(expectedResponse);
+
+        Page<CategoryResponseDto> result = categoryService.findAll(pageable, "");
+
+        Assertions.assertEquals(1, result.getTotalElements());
+        Assertions.assertEquals(expectedResponse.id(), result.getContent().get(0).id());
+    }
+
+    @Test
+    void delete() {
+        Category category = createCategory();
+
+        when(categoryRepository
+                .findById(DEFAULT_ID))
+                .thenReturn(Optional.of(category));
+
+        categoryService.delete(DEFAULT_ID);
+
+        verify(categoryRepository).delete(category);
+    }
+
+    @Test
+    void deleteThrowsException() {
+        when(categoryRepository
+                .findById(DEFAULT_ID))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(
+                CategoryException.class, () -> categoryService.delete(DEFAULT_ID)
+        );
+    }
+
+    @Test
+    void create() {
+        CategoryCreateRequestDto request = createCategoryCreateRequestDto();
+        Category category = createCategory();
+        CategoryResponseDto expectedResponse = createCategoryResponseDto();
+
+        when(categoryRepository.findByName(request.name()))
+                .thenReturn(Optional.empty());
+        when(categoryCreateRequestDtoMapper.toEntity(request))
+                .thenReturn(category);
+        when(categoryRepository.save(category))
+                .thenReturn(category);
+        when(categoryResponseDtoMapper.toDto(category))
+                .thenReturn(expectedResponse);
+
+        CategoryResponseDto result = categoryService.create(request);
+
+        Assertions.assertEquals(expectedResponse, result);
+    }
+
+    @Test
+    void createThrowsExceptionWhenNameInUse() {
+        CategoryCreateRequestDto request = createCategoryCreateRequestDto();
+        Category existingCategory = createCategory();
+
+        when(categoryRepository.findByName(request.name()))
+                .thenReturn(Optional.of(existingCategory));
+
+        Assertions.assertThrows(
+                CategoryException.class, () -> categoryService.create(request)
+        );
+    }
+
+    @Test
+    void update() {
+        CategoryUpdateRequestDto request = createCategoryUpdateRequestDto();
+        Category category = createCategory();
+        CategoryResponseDto expectedResponse = createCategoryResponseDto();
+
+        when(categoryRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.of(category));
+        when(categoryRepository.findByName(request.name()))
+                .thenReturn(Optional.empty());
+        when(categoryRepository.save(category))
+                .thenReturn(category);
+        when(categoryResponseDtoMapper.toDto(category))
+                .thenReturn(expectedResponse);
+
+        CategoryResponseDto result = categoryService.update(request);
+
+        Assertions.assertEquals(expectedResponse, result);
+    }
+
+    @Test
+    void updateThrowsExceptionWhenCategoryNotFound() {
+        CategoryUpdateRequestDto request = createCategoryUpdateRequestDto();
+
+        when(categoryRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(
+                CategoryException.class, () -> categoryService.update(request)
+        );
+    }
+
+    @Test
+    void updateThrowsExceptionWhenNameInUse() {
+        CategoryUpdateRequestDto request = createCategoryUpdateRequestDto();
+        Category category = createCategory();
+        category.setId(DEFAULT_ID);
+        Category existingCategory = new Category();
+        existingCategory.setName(request.name());
+
+        when(categoryRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.of(category));
+        when(categoryRepository.findByName(request.name()))
+                .thenReturn(Optional.of(existingCategory));
+
+        Assertions.assertThrows(
+                CategoryException.class, () -> categoryService.update(request)
+        );
+    }
+
+    private CategoryCreateRequestDto createCategoryCreateRequestDto() {
+        return new CategoryCreateRequestDto(
+                DEFAULT_NAME,
+                DEFAULT_DESCRIPTION
+        );
+    }
+
+    private CategoryResponseDto createCategoryResponseDto() {
+        return new CategoryResponseDto(
+                DEFAULT_ID,
+                DEFAULT_NAME,
+                DEFAULT_DESCRIPTION
+        );
+    }
+
+    private Category createCategory() {
+        return new Category(
+                DEFAULT_ID,
+                DEFAULT_NAME,
+                DEFAULT_DESCRIPTION,
+                null
+        );
+    }
+
+    private CategoryUpdateRequestDto createCategoryUpdateRequestDto() {
+        return new CategoryUpdateRequestDto(
+                DEFAULT_ID,
+                DEFAULT_NAME,
+                DEFAULT_DESCRIPTION
+        );
+    }
+
+}
---
+++ src/test/java/com/stepanew/senlaproject/service/UserServiceImplTest.java
@@ -0,0 +1,278 @@
+package com.stepanew.senlaproject.service;
+
+import com.stepanew.senlaproject.domain.dto.request.UserCreateRequestDto;
+import com.stepanew.senlaproject.domain.dto.request.UserUpdateMeRequestDto;
+import com.stepanew.senlaproject.domain.dto.response.UserCreatedResponseDto;
+import com.stepanew.senlaproject.domain.dto.response.UserUpdateMeResponseDto;
+import com.stepanew.senlaproject.domain.entity.Profile;
+import com.stepanew.senlaproject.domain.entity.User;
+import com.stepanew.senlaproject.domain.mapper.profile.CreateProfileRequestDtoMapper;
+import com.stepanew.senlaproject.domain.mapper.user.UserCreateRequestDtoMapper;
+import com.stepanew.senlaproject.domain.mapper.user.UserCreateResponseDtoMapper;
+import com.stepanew.senlaproject.domain.mapper.user.UserUpdateMeResponseDtoMapper;
+import com.stepanew.senlaproject.exceptions.AuthException;
+import com.stepanew.senlaproject.exceptions.UserException;
+import com.stepanew.senlaproject.repository.UserRepository;
+import com.stepanew.senlaproject.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserCreateRequestDtoMapper userCreateRequestDtoMapper;
+
+    @Mock
+    private UserUpdateMeResponseDtoMapper userUpdateMeResponseDtoMapper;
+
+    @Mock
+    private UserCreateResponseDtoMapper userCreateResponseDtoMapper;
+
+    @Mock
+    private CreateProfileRequestDtoMapper createProfileRequestDtoMapper;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    private static final Long DEFAULT_ID = 1L;
+
+    private static final String DEFAULT_EMAIL = "test@example.com";
+
+    private static final String DEFAULT_PASSWORD = "password";
+
+    private static final String DEFAULT_FIRST_NAME = "Ivan";
+
+    private static final String DEFAULT_LAST_NAME = "Ivanov";
+
+    private static final String DEFAULT_PATRONYMIC = "Ivanovich";
+
+    private static final String ENCODED_PASSWORD = "encodedPassword";
+
+    @Test
+    void getById() {
+        User user = createUser();
+
+        when(userRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.of(user));
+
+        User result = userService.getById(DEFAULT_ID);
+
+        verify(userRepository).findById(DEFAULT_ID);
+        Assertions.assertEquals(DEFAULT_ID, result.getId());
+    }
+
+    @Test
+    void getByIdThrowsException() {
+        when(userRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.empty());
+
+        assertThrowsUserException(() -> userService.getById(DEFAULT_ID));
+
+    }
+
+    @Test
+    void getByEmail() {
+        User user = createUser();
+
+        when(userRepository.findByEmail(DEFAULT_EMAIL))
+                .thenReturn(Optional.of(user));
+
+        User result = userService.getByEmail(DEFAULT_EMAIL);
+
+        verify(userRepository).findByEmail(DEFAULT_EMAIL);
+        Assertions.assertEquals(DEFAULT_EMAIL, result.getEmail());
+    }
+
+    @Test
+    void getByEmailThrowsException() {
+        when(userRepository.findByEmail(DEFAULT_EMAIL))
+                .thenReturn(Optional.empty());
+
+        assertThrowsAuthException(() -> userService.getByEmail(DEFAULT_EMAIL));
+    }
+
+    @Test
+    void create() {
+        UserCreateRequestDto request = createUserCreateRequestDto();
+        User user = createUser();
+        UserCreatedResponseDto response = createUserCreatedResponseDto();
+
+        when(userCreateRequestDtoMapper.toEntity(request))
+                .thenReturn(user);
+        when(userRepository.findByEmail(DEFAULT_EMAIL))
+                .thenReturn(Optional.empty());
+        when(passwordEncoder.encode(DEFAULT_PASSWORD))
+                .thenReturn(ENCODED_PASSWORD);
+        when(userCreateResponseDtoMapper.toDto(user))
+                .thenReturn(response);
+        when(createProfileRequestDtoMapper.toEntity(request))
+                .thenReturn(
+                        new Profile(
+                                DEFAULT_ID,
+                                DEFAULT_FIRST_NAME,
+                                DEFAULT_LAST_NAME,
+                                DEFAULT_PATRONYMIC,
+                                user)
+                );
+
+        UserCreatedResponseDto result = userService.create(request);
+
+        verify(userRepository).save(user);
+        Assertions.assertEquals(response, result);
+    }
+
+    @Test
+    void createThrowsExceptionWhenEmailInUse() {
+        UserCreateRequestDto request = createUserCreateRequestDto();
+        User existingUser = createUser();
+
+        when(userCreateRequestDtoMapper.toEntity(request))
+                .thenReturn(existingUser);
+        when(userRepository.findByEmail(DEFAULT_EMAIL))
+                .thenReturn(Optional.of(existingUser));
+
+        assertThrowsAuthException(() -> userService.create(request));
+    }
+
+    @Test
+    void createThrowsExceptionWhenPasswordsDoNotMatch() {
+        UserCreateRequestDto request = new UserCreateRequestDto(
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_PATRONYMIC,
+                DEFAULT_PASSWORD,
+                ENCODED_PASSWORD,
+                DEFAULT_EMAIL
+        );
+        User user = createUser();
+
+        when(userCreateRequestDtoMapper.toEntity(request))
+                .thenReturn(user);
+
+        assertThrowsAuthException(() -> userService.create(request));
+    }
+
+    @Test
+    void updateMe() {
+        UserUpdateMeRequestDto request = createUserUpdateMeRequestDto();
+        User user = createUser();
+
+        when(userRepository.findByEmail(DEFAULT_EMAIL))
+                .thenReturn(Optional.of(user));
+        when(userUpdateMeResponseDtoMapper.toDto(user))
+                .thenReturn(createUserUpdateMeResponseDto());
+
+        UserUpdateMeResponseDto result = userService.updateMe(request, DEFAULT_EMAIL);
+
+        verify(userRepository).save(user);
+        Assertions.assertEquals(DEFAULT_FIRST_NAME, result.firstName());
+        Assertions.assertEquals(DEFAULT_LAST_NAME, result.lastName());
+        Assertions.assertEquals(DEFAULT_PATRONYMIC, result.patronymic());
+    }
+
+    @Test
+    void updateById() {
+        UserUpdateMeRequestDto request = createUserUpdateMeRequestDto();
+        User user = createUser();
+
+        when(userRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.of(user));
+        when(userUpdateMeResponseDtoMapper.toDto(user))
+                .thenReturn(createUserUpdateMeResponseDto());
+
+        UserUpdateMeResponseDto result = userService.updateById(request, DEFAULT_ID);
+
+        verify(userRepository).save(user);
+        Assertions.assertEquals(DEFAULT_FIRST_NAME, result.firstName());
+        Assertions.assertEquals(DEFAULT_LAST_NAME, result.lastName());
+        Assertions.assertEquals(DEFAULT_PATRONYMIC, result.patronymic());
+    }
+
+    @Test
+    void updateByIdThrowsExceptionWhenUserNotFound() {
+        UserUpdateMeRequestDto request = createUserUpdateMeRequestDto();
+
+        when(userRepository.findById(DEFAULT_ID))
+                .thenReturn(Optional.empty());
+
+        assertThrowsUserException(() -> userService.updateById(request, DEFAULT_ID));
+    }
+
+    private UserUpdateMeResponseDto createUserUpdateMeResponseDto() {
+        return new UserUpdateMeResponseDto(
+                DEFAULT_EMAIL,
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_PATRONYMIC
+        );
+    }
+
+    private static UserUpdateMeRequestDto createUserUpdateMeRequestDto() {
+        return new UserUpdateMeRequestDto(
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_PATRONYMIC
+        );
+    }
+
+    private UserCreatedResponseDto createUserCreatedResponseDto() {
+        return new UserCreatedResponseDto(
+                DEFAULT_ID,
+                DEFAULT_EMAIL,
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_PATRONYMIC
+        );
+    }
+
+    private UserCreateRequestDto createUserCreateRequestDto() {
+        return new UserCreateRequestDto(
+                DEFAULT_FIRST_NAME,
+                DEFAULT_LAST_NAME,
+                DEFAULT_PATRONYMIC,
+                DEFAULT_PASSWORD,
+                DEFAULT_PASSWORD,
+                DEFAULT_EMAIL
+        );
+    }
+
+    private User createUser() {
+        return new User(
+                DEFAULT_ID,
+                DEFAULT_PASSWORD,
+                DEFAULT_EMAIL,
+                null,
+                null,
+                new Profile()
+        );
+    }
+
+    private void assertThrowsUserException(Runnable action) {
+        Assertions.assertThrows(
+                UserException.class, action::run
+        );
+    }
+
+    private void assertThrowsAuthException(Runnable action) {
+        Assertions.assertThrows(
+                AuthException.class, action::run
+        );
+    }
+}
